37e1124d7f9f280e208de304841d16111a6ea554
Quang Le <phuc.quang102@gmail.com>
2021-12-16 17:58:01 +0700

    Fix: read test input

--- app/src/main/kotlin/adventofcode/Input.kt
+++ app/src/main/kotlin/adventofcode/Input.kt
@@ -4,7 +4,7 @@ class Input {
     companion object {
         fun forDate(year: Int, day: Int): String {
             val testPostfix = System.getProperty("testpostfix", "")
-            val res = this::class.java.classLoader.getResource(String.format("y%d/input%02d.txt", year, day, testPostfix))
+            val res = this::class.java.classLoader.getResource(String.format("y%d/input%02d%s.txt", year, day, testPostfix))
             return res?.readText()?:""
         }
     }
